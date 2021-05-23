7016cf2793009311320b3a90e811cf486fa5ebad
ShinSanha <33896511+SahhaShin@users.noreply.github.com>
2021-05-23 17:48:14 +0900

    21.05.23

---
+++ 2941번 크로아티아 알파벳.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[58]:
+
+
+result=0
+word=0
+cro=["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="]
+sentence=input()
+for i in cro:
+    if i == "z=":
+        result=result+sentence.count(i)-sentence.count("dz=")
+        word=word+len(i)*(sentence.count(i)-sentence.count("dz="))
+        
+    else:
+        result=result+sentence.count(i)
+        word=word+len(i)*sentence.count(i)
+        
+print(result+len(sentence)-word)
+
