61cfdb069d5d883ee2754914d3f6645194afad80
ShinSanha <33896511+SahhaShin@users.noreply.github.com>
2021-05-23 17:47:49 +0900

    21.05.23

---
+++ 5622번 다이얼.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+dr={'A':2, 'B':2, 'C':2, 'D':3, 'E':3, 'F':3, 'G':4, 'H':4, 'I':4, 'J':5, 'K':5, 'L':5, 'M':6, 'N':6, 'O':6, 'P':7, 'Q':7, 'R':7, 'S':7, 'T':8, 'U':8, 'V':8, 'W':9, 'X':9, 'Y':9,'Z':9}
+result=0
+sentence=list(input())
+for i in sentence:
+    result=result+dr[i]
+print(result+len(sentence))
+
+
+# In[ ]:
+
+
+
+
