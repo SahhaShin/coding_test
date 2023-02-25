e6359de9a03d89e2ed48f774a0c6c7b6c4918fec
ShinSanha <33896511+SahhaShin@users.noreply.github.com>
2023-02-26 02:54:46 +0900

    [Bronze II] Title: 방 배정, Time: 208 ms, Memory: 17516 KB -BaekjoonHub

---
+++ 백준/Bronze/13300. 방 배정/방 배정.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		//1. 수학여행 참가수 / 방 최대 인원
+		int entry = sc.nextInt();
+		int room_max=sc.nextInt();
+
+		int[][] room = new int[2][7]; //gender / grade
+		
+		//2. 성별 / 학년
+		for(int i=0;i<entry;i++) {
+			int gender=sc.nextInt();
+			int grade = sc.nextInt();
+			room[gender][grade]++;
+		}
+		
+		//3. 최소한의 방의 갯수
+		int result=0;
+		for(int i=0;i<room.length;i++) {
+			for(int j=0;j<room[0].length;j++) {
+				if(room[i][j]%2==0) result+=room[i][j]/2;
+				else result+=room[i][j]/2+room[i][j]%2; //ex 5 -> 2+3 / 1 -> 0+1
+			}
+		}
+		
+		System.out.println(result);
+		
+	}
+
+}
