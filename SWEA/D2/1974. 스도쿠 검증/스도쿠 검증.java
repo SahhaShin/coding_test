5237ccb75e955a2c0f3e22e590673e50dc6f593d
ShinSanha <33896511+SahhaShin@users.noreply.github.com>
2023-03-03 11:41:07 +0900

    [D2] Title: 스도쿠 검증, Time: 155 ms, Memory: 23,672 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.java
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.java
@@ -1,90 +1,79 @@
-import java.util.*;
-import java.io.*;
+import java.util.Scanner;
 
-class Solution
-{
-    
-	public static void main(String args[]) throws Exception
-	{
+public class Solution {
+	static int[][] arr=new int[9][9];;
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
 		//1. test case
 		int t = sc.nextInt();
 		
 		for(int T=1;T<t+1;T++) {
-			int result=1;
-			//2.배열
-			int[][] arr = new int[9][9];
+			//2. 배열
 			for(int i=0;i<9;i++) {
 				for(int j=0;j<9;j++) {
 					arr[i][j]=sc.nextInt();
 				}
 			}
-			if(!row_check(arr)) result=0;
-			if(!col_check(arr)) result=0;
-			if(!mini_check (arr)) result=0;
+			
+			//3. 검사
+			//가로
+			int result=1;
+			if(!row_check()) result=0;
+			
+			//세로
+			if(!col_check()) result=0;
+			
+			//3x3
+			if(!three_check()) result=0;
 			
 			System.out.println("#"+T+" "+result);
 			
 		}//test case end
-	}
-    
-    static public boolean row_check (int[][] arr) {
-		boolean result=true;
-		
-		for(int i=0;i<arr.length;i++) {
-			int[] count = new int[10];
-			for(int j=0;j<arr.length;j++) {
-				if(count[arr[i][j]]==0) {
-					count[arr[i][j]]++;
-				}
-				else {
-					result=false;
-					break;
-				}		
-			}
-		}
-		return result;	
+
 	}
 	
-	static public boolean col_check (int[][] arr) {
-		boolean result=true;
-		
-		for(int i=0;i<arr.length;i++) {
+	static public boolean row_check() {
+		for(int i=0;i<9;i++) {
 			int[] count = new int[10];
-			for(int j=0;j<arr.length;j++) {
-				if(count[arr[j][i]]==0) {
-					count[arr[j][i]]++;
-				}
-				else {
-					result=false;
-					break;
-				}		
+			for(int j=0;j<9;j++) {
+				if(count[arr[i][j]]==0)count[arr[i][j]]++;
+				else return false;
 			}
 		}
-		return result;	
+		return true;
 	}
 	
-	static public boolean mini_check (int[][] arr) {
-		boolean result=true;
-		int row=0;//start
-		int col=0;//start 
-		//전체 9번
+	static public boolean col_check() {
+		for(int i=0;i<9;i++) {
+			int[] count = new int[10];
+			for(int j=0;j<9;j++) {
+				if(count[arr[j][i]]==0)count[arr[j][i]]++;
+				else return false;
+			}
+		}
+		return true;
+	}
+	
+	static public boolean three_check() {
+		int row=0;
+		int col=0;
 		for(int i=0;i<9;i++) {
 			int[] count = new int[10];
 			for(int j=0;j<3;j++) {
 				for(int k=0;k<3;k++) {
-					if(count[arr[row+j][col+k]]==0)count[arr[row][col]]++;
-                    else {
-						result=false;
-						break;
-					}	
+					if(count[arr[j+row][k+col]]==0)count[arr[j+row][k+col]]++;
+                    else return false;
 				}
 			}
-			if(col!=6) col+=3;
-			else {row+=3; col=0;}
+			col+=3;
+			if(col>8) {
+				//3x3밑으로 이동 
+				col=0;
+				row+=3;
+			}
 		}
-		
-		return result;	
+		return true;
 	}
+
 }
