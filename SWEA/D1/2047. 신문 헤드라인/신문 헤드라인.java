60dd40ba75ee9e88a0eaa50fa19cdd46c48938fa
ShinSanha <33896511+SahhaShin@users.noreply.github.com>
2023-03-09 22:26:07 +0900

    [D1] Title: 신문 헤드라인, Time: 128 ms, Memory: 20,064 KB -BaekjoonHub

---
+++ SWEA/D1/2047. 신문 헤드라인/신문 헤드라인.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(str.toUpperCase());
+	}
+}
