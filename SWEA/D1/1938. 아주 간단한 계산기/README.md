6314d9b24e92366f203ea68924d4dd474ba7d908
ShinSanha <33896511+SahhaShin@users.noreply.github.com>
2023-03-15 22:18:21 +0900

    [D1] Title: 아주 간단한 계산기, Time: 126 ms, Memory: 20,464 KB -BaekjoonHub

---
+++ SWEA/D1/1938. 아주 간단한 계산기/README.md
@@ -0,0 +1,11 @@
+# [D1] 아주 간단한 계산기 - 1938 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5PjsYKAMIDFAUq) 
+
+### 성능 요약
+
+메모리: 20,464 KB, 시간: 126 ms, 코드길이: 371 Bytes
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D1/1938. 아주 간단한 계산기/아주 간단한 계산기.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+	}
+}
