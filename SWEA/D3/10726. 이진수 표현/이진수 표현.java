08f1de3500294a8c0ed192418cba07ada3dc3d3e
ShinSanha <33896511+SahhaShin@users.noreply.github.com>
2023-03-20 19:25:35 +0900

    [D3] Title: 이진수 표현, Time: 374 ms, Memory: 54,364 KB -BaekjoonHub

--- SWEA/D3/10726. 이진수 표현/이진수 표현.java
+++ SWEA/D3/10726. 이진수 표현/이진수 표현.java
@@ -11,7 +11,7 @@ public class Solution {
             int N =sc.nextInt();//마지막 N비트 
             int num = sc.nextInt();//10진수
             
-            //비트 1의 자리가 띄워져 있으면 홀수니까 <<를 N번하는 동안 모두 홀수면 ok
+            //비트 1의 자리가 띄워져 있으면 홀수니까 >>를 N번하는 동안 모두 홀수면 ok
             String result="ON";
             for(int i=0;i<N;i++) {
             	if(num%2!=1) {
