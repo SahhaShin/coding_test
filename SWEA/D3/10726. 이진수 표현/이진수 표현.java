f93ec2888235a9655c462e81d5c3b9db9cce5f5f
ShinSanha <33896511+SahhaShin@users.noreply.github.com>
2023-03-20 19:24:28 +0900

    [D3] Title: 이진수 표현, Time: 374 ms, Memory: 54,364 KB -BaekjoonHub

--- SWEA/D3/10726. 이진수 표현/이진수 표현.java
+++ SWEA/D3/10726. 이진수 표현/이진수 표현.java
@@ -1,38 +1,28 @@
 import java.util.Scanner;
 
 public class Solution {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int t = sc.nextInt();
-		
-		for(int T=1;T<t+1;T++) {
-			int group=sc.nextInt();//마지막 몇개가 1로 켜져있는지
-			int num = sc.nextInt();//10진수
-			
-			//2진수 구하기
-			StringBuilder bi = new StringBuilder();
-			while(num>0) {
-				bi.append(num%2);
-				num/=2;
-			}
-			
-			//거꾸로
-			bi.reverse();
-			
-			//판별해야하는 1의 갯수보다 2진수 길이가 적으면 OFF
-			String result = "ON";
-			if(bi.length()<group) result = "OFF";
-			else {
-				//그게 아니면 길이만큼 조사해서 1인지 판단
-				for(int i=bi.length()-1;i>(bi.length()-1)-group;i--) {
-					if(bi.charAt(i)!='1') {
-						result = "OFF";
-						break;
-					}
-				}
-			}
-			System.out.println("#"+T+" "+result);
-		}//test case end
-	}
+    //비트 버전
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+         
+        int t = sc.nextInt();
+         
+        for(int T=1;T<t+1;T++) {
+            int N =sc.nextInt();//마지막 N비트 
+            int num = sc.nextInt();//10진수
+            
+            //비트 1의 자리가 띄워져 있으면 홀수니까 <<를 N번하는 동안 모두 홀수면 ok
+            String result="ON";
+            for(int i=0;i<N;i++) {
+            	if(num%2!=1) {
+            		result="OFF";
+            		break;
+            	}
+            	num=num>>1;
+            	
+            }
+             
+            System.out.println("#"+T+" "+result);
+        }//test case end
+    }
 }
