78ef66bb194ae6785dba4609c11e58cade215988
ShinSanha <33896511+SahhaShin@users.noreply.github.com>
2023-04-14 23:22:42 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 256 ms, Memory: 15948 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc =  new Scanner(System.in);
+		
+		int N = sc.nextInt();//1~N명의 사람
+		int K = sc.nextInt();//K번째 사람 제거
+		
+		boolean[] index = new boolean[N+1];
+		
+		System.out.printf("<");
+		
+		int jump=0;
+		int targetNum=K;
+		
+		for(int i=0;i<N;i++) {
+			if(i==N-1) {
+				System.out.printf(targetNum+">");
+			}
+			else System.out.printf(targetNum+", ");
+			index[targetNum]=true;
+			
+			while(jump!=K && i!=N-1) {
+				targetNum=(targetNum+1)%N;
+				if(targetNum==0)targetNum=N;
+				if(index[targetNum]==false) jump++;
+			}
+			jump=0;
+		}
+	}
+
+}
