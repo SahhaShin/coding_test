fc2211e3de6f375157986ffbd61d896c7ae83574
ShinSanha <33896511+SahhaShin@users.noreply.github.com>
2023-04-16 15:40:10 +0900

    [Silver IV] Title: 설탕 배달, Time: 108 ms, Memory: 12876 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,39 +1,21 @@
 import java.util.Scanner;
 
 public class Main {
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();//18
-		int N_copy=0;
-		int count=0;
-		int idx=0;
 		
-		//만약에 N이 5로 딱 나눠 떨어지면 결과 쉽게 나옴
-		if(N%5==0) count=N/5;
+		int N = sc.nextInt();
 		
-		else if(N/5>0) {
-			for(int i=N/5;i>=0;i--) {
-				//copy본을 사용하는 이유는 N을 나눌 5*i의 적당한 수를 찾기 위함이다. 
-				N_copy=N-(5*i); //3 2 1
-				//5로 나눌 수 있는 가짓수를 시험하며 그때마다 3으로 나눠지면 ok
-				//그러기 위해 높은 5 배수부터 시작하는 것임
-				if(N_copy%3==0) {
-					count=i+N_copy/3;//5로 나눈 횟수와 3으로 나눈 횟수
-					break;//총 결과 끝나고 바로 끝내줘야함 안그럼 계속 돌아가서 정답 갱신
-				}
-				else {
-					count=-1;
-				}
-			}
-			
+		int[] dp = new int[N+1];
+		dp[3] = 1;
+		if(N>=5) dp[5]=1;
+		
+		for(int i=6;i<=N;i++) {
+			if(dp[i-3]!=0 && dp[i-5]==0) dp[i] = dp[i-3]+1;
+			if(dp[i-3]==0 && dp[i-5]!=0) dp[i] = dp[i-5]+1;
+			if(dp[i-3]!=0 && dp[i-5]!=0) dp[i] = Math.min(dp[i-3]+1, dp[i-5]+1);
 		}
-		else if(N%3==0) count=N/3;
-		
-		else count=-1;
-		
-		System.out.println(count);
-
+		if(dp[N]==0) dp[N]=-1;
+		System.out.println(dp[N]);
 	}
-
 }
