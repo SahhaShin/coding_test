707c9e0a2129a531d2b4185c34fc46cac5d12ce9
ShinSanha <33896511+SahhaShin@users.noreply.github.com>
2023-04-22 12:20:15 +0900

    [Bronze V] Title: 꼬마 정민, Time: 112 ms, Memory: 12856 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long num1 = sc.nextLong();
+		long num2 = sc.nextLong();
+		long num3 = sc.nextLong();
+		
+		System.out.println(num1+num2+num3);
+	}
+
+}
