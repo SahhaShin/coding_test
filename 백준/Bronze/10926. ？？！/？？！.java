288b07da2eabe92e150b1b7f801827ab28510494
ShinSanha <33896511+SahhaShin@users.noreply.github.com>
2023-04-22 12:11:20 +0900

    [Bronze V] Title: ??!, Time: 108 ms, Memory: 12928 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String stm = sc.next();
+		System.out.println(stm+"??!");
+	}
+}
