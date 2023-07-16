92af397fc913795c061d6030e98691a2b8ecbf96
ShinSanha <33896511+SahhaShin@users.noreply.github.com>
2023-07-16 22:49:01 +0900

    [Gold V] Title: 집합의 표현, Time: 2356 ms, Memory: 236004 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -2,64 +2,74 @@ import java.util.Scanner;
 
 public class Main {
 
-	static int[] p,rank;
-	static int N,M;
+	static int maxNum;
+	static int[] parent;
+	static int[] rank;
+	
+	static int calCount;
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		M = sc.nextInt();
 		
-		//초기화
-		p = new int[N+1];
-		rank = new int[N+1];
+		maxNum = sc.nextInt();
+		parent = new int[maxNum+1];
+		rank = new int[maxNum+1];
 		
-		//구현 시작
-		makeset();
+		calCount = sc.nextInt();
 		
-		for(int i=0;i<M;i++) {
-			int mode = sc.nextInt();//0이면 union 1이면 부모 같은지 체크 
-			if(mode==0) {
-				int node1 = sc.nextInt();
-				int node2 = sc.nextInt();
-				union(node1,node2);
-			}
-			else if(mode==1) {
-				int node1P = myP(sc.nextInt());
-				int node2P = myP(sc.nextInt());
-				if(node1P==node2P)System.out.println("YES");
+		
+		//시작 전 부모 초기 셋팅
+		setP();
+		
+		for(int T=1;T<=calCount;T++) {
+			int mode = sc.nextInt(); //0이면 합집합모드, 1이면 확인모드
+			
+			int num1 = sc.nextInt();
+			int num2 = sc.nextInt();
+			
+			if(mode == 0) {
+				//합집합 모드
+				if(findP(num1)==findP(num2)) continue;
+				
+				union(num1, num2);
+			}else if(mode == 1) {
+				if(findP(num1)==findP(num2)) System.out.println("YES");
 				else System.out.println("NO");
 			}
+			
+		}
+
+	}
+	
+	//크루스칼
+	
+	//부모를 자기 자신으로 셋팅
+	public static void setP() {
+		for(int i=1;i<=maxNum;i++) {
+			parent[i]=i;
 		}
 	}
 	
-	//자기 자신을 부모로 만든다. 
-	public static void makeset() {
-		for(int i=1;i<=N;i++) {
-			p[i]=i;
-			rank[i]=0;
-		}
+	//자식의 부모를 찾아준다
+	public static int findP(int child) {
+		if(parent[child]==child) return child;
+		else return findP(parent[child]);
 	}
 	
-	//부모 찾기 
-	public static int myP(int node) {
-		if(p[node]==node) return node;
-		return p[node]=myP(p[node]);
-	}
-	
-	//서로 부모 합치기
-	public static void union(int node1, int node2) {
-		int a = myP(node1);
-		int b = myP(node2);
+	//합집합
+	public static void union(int c1, int c2) {
+		int c1_p = findP(c1);
+		int c2_p = findP(c2);
 		
-		if(a==b) return;
-		else {
-			if(rank[a]>rank[b]) {
-				p[b]=a;
-			}
-			else {
-				p[a]=b;
-				if(rank[a]==rank[b]) rank[b]++;
-			}
+		//둘의 부모가 다른 것만 들어온다고 가정
+		
+		if(rank[c1_p]>=rank[c2_p]) {
+			parent[c2_p] = c1_p;
+			rank[c1_p]++;
+		}else {
+			parent[c1_p] = c2_p;
+			rank[c2_p]++;
 		}
 	}
+
 }
