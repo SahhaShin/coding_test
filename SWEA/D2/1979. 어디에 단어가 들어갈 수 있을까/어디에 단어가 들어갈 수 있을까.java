5aa78fa1d83c5fe4a52bedce7aeadd7f041053c8
ShinSanha <33896511+SahhaShin@users.noreply.github.com>
2023-10-09 20:53:44 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 165 ms, Memory: 25,324 KB -BaekjoonHub

--- SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.java
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.java
@@ -1,78 +1,94 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Solution {
+	
+	static int[][] arr;
+	static int N;
+	static int K;
+	static int result;
 
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
 		
-		//1. test case
-		int t = sc.nextInt();
-		for(int T=1;T<t+1;T++) {
-			//2. 퍼즐 크기 N
-			int N = sc.nextInt();
-			
-			//3. 단어 길이
-			int len = sc.nextInt();
-			
-			//4. 퍼즐 정보
-			int[][] p = new int[N][N];
-			for(int i=0;i<p.length;i++) {
-				for(int j=0;j<p.length;j++) {
-					p[i][j]=sc.nextInt();
-				}
-			}
-			
-			//5. 단어 길이가 들어갈 수 있는 공간은 몇 개인가 
-			//가로 탐색
-			int result=0;
-			for(int i=0;i<N;i++) {
-				int count=0;//1의 길이를 세줄 것 
-				for(int j=0;j<N;j++) {
-					if(p[i][j]==1) {
-						//1을 만나면 자리 ++
-						count++;
-						//단어길이와 같아지면 result++
-						if(count==len) result++;
-						if(count>len) {
-							//단어 길이보다 커지면 이전 result값을 지워준다.
-							//다시 초기화 
-							result--;
-							count=0;
-						}
-					}
-					else {
-						//0을 만나면 초기화 
-						count=0;
-					}
-				}
-			}
-			
-			//세로 탐색 
+		int test = sc.nextInt();
+		for(int T=1;T<=test;T++) {
+			N = sc.nextInt();
+			K = sc.nextInt();
+			arr = new int[N][N];
+			result = 0;
 			
 			for(int i=0;i<N;i++) {
-				int count=0;//1의 길이를 세줄 것 
 				for(int j=0;j<N;j++) {
-					if(p[j][i]==1) {
-						//1을 만나면 자리 ++
-						count++;
-						//단어길이와 같아지면 result++
-						if(count==len) result++;
-						if(count>len) {
-							//단어 길이보다 커지면 이전 result값을 지워준다.
-							//다시 초기화 
-							result--;
-							count=0;
-						}
-					}
-					else {
-						//0을 만나면 초기화 
-						count=0;
-					}
+					arr[i][j]=sc.nextInt();
 				}
 			}
+			
+			//입력 끝
+			
+			rowCount();
+			colCheck();
+			
 			System.out.println("#"+T+" "+result);
-		}//test case end
+		}
 
 	}
-
+	
+	public static boolean check(int count, int K) {
+		if(count==K) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	//가로 찾기
+	public static void rowCount() {
+		for(int i=0;i<N;i++) {
+			
+			int count = 0;
+			
+			for(int j=0;j<N;j++) {
+				if(arr[i][j]==1) {
+					//하얀공간
+					count++;
+				}
+				
+				if(arr[i][j]==0 || j==N-1) {
+					//검정공간 && count가 K만큼 모임 
+					//다음줄로 넘어가기 전에 체크 
+					if(check(count, K)) {
+						result++;
+					}
+					count=0;
+				}
+			}
+		}
+	}
+	
+	
+	//세로 찾기
+	public static void colCheck() {
+		for(int i=0;i<N;i++) {
+			
+			int count = 0;
+			
+			for(int j=0;j<N;j++) {
+				if(arr[j][i]==1) {
+					//하얀공간
+					count++;
+				}
+				
+				if(arr[j][i]==0 || j==N-1) {
+					//검정공간 && count가 K만큼 모임 
+					//다음줄로 넘어가기 전에 체크 
+					if(check(count, K)) {
+						result++;
+					}
+					count=0;
+				}
+			}
+		}
+	}
 }
