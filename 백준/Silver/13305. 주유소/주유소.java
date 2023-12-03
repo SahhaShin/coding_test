724068f93686f8ce05280bd261a9025c7e102d2f
ShinSanha <33896511+SahhaShin@users.noreply.github.com>
2023-12-03 23:45:12 +0900

    [Silver III] Title: 주유소, Time: 1040 ms, Memory: 171624 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -6,8 +6,8 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		
 		int country = sc.nextInt();
-		int[] distance = new int[country-1];
-		int[] fee = new int[country];
+		long[] distance = new long[country-1];
+		long[] fee = new long[country];
 		
 		for(int i=0;i<country-1;i++) {
 			distance[i]=sc.nextInt();
@@ -17,14 +17,16 @@ public class Main {
 			fee[i]=sc.nextInt();
 		}
 		
-		int result=0;
+		long result=0;
+		long minFee=Long.MAX_VALUE;
+		
 		for(int i=0;i<country-1;i++) {
-			if(fee[i]<fee[i+1]) {
-				result += (distance[i]+distance[i+1])*fee[i];//내가 다음 도시 비용보다 더 작다면 다음 도시 것도 같이
-				i++;//다음도시는 이미 계산 했으니 그 다음 도시로 
+			if(fee[i]<minFee) {
+				minFee=fee[i];
+				result += distance[i]*fee[i];
 			}
 			else {
-				result += (distance[i])*fee[i];//내가 다음 도시 비용보다 더 크다면 내꺼만
+				result += distance[i]*minFee;
 			}
 		}
 		
