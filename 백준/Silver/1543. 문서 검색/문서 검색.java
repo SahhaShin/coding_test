03384aa6a1088112daf951f0cc9d3c29644ed78a
ShinSanha <33896511+SahhaShin@users.noreply.github.com>
2024-03-06 11:05:39 +0900

    [Silver V] Title: 문서 검색, Time: 84 ms, Memory: 11496 KB -BaekjoonHub

---
+++ 백준/Silver/1543. 문서 검색/문서 검색.java
@@ -0,0 +1,32 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+        String search = br.readLine();
+
+        //투포인터
+        int S = 0;
+        int E = 0;
+        int search_order = 0;
+
+        int count=0;
+        while(E<line.length()){
+            char cur = line.charAt(E);
+            if(cur == search.charAt(search_order)){
+                if(search_order == search.length()-1){
+                    count++;
+                    search_order = 0;
+                    S = E+1;
+                }else search_order++;
+                E++;
+            }else{
+                S++;
+                E = S;
+                search_order = 0;
+            }
+        }
+        System.out.println(count);
+    }
+}
