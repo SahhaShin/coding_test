31307d9f95ed74fe821e6e2d84ce07daa573f73d
ShinSanha <33896511+SahhaShin@users.noreply.github.com>
2024-03-11 11:57:51 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 100 ms, Memory: 12208 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,31 +1,30 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
-public class Main {
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int N = sc.nextInt();//수열의 크기
-		int[] arr = new int[N];
-		
-		for(int i=0;i<N;i++) {
-			arr[i]=sc.nextInt();
-		}
-		
-		int[] dp = new int[N];
-		Arrays.fill(dp, 1);//모두 자기 자신의 최장길이는 1이다.
-		int max=1;
-		
-		for(int i=1;i<N;i++) {
-			for(int j=0;j<i;j++) {
-				if(arr[j]<arr[i]) {
-					dp[i]=Math.max(dp[j]+1,dp[i]);
-				}
-			}
-			if(max<dp[i])max=dp[i];
-		}
-		
-		System.out.println(max);
-	}
+        String nums = br.readLine();
+        StringTokenizer st = new StringTokenizer(nums," ");
+        int[] num = new int[N];
+        for(int i=0; i<N; i++){
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+
+        //입력 끝
+
+        int[] dp = new int[N];
+        int max = 1;
+        Arrays.fill(dp, 1);
+        for(int i=1; i<N; i++){
+            for(int j=0; j<i; j++){
+                if(num[i]>num[j]) dp[i] = Math.max(dp[i], dp[j]+1);
+            }
+            if(max<dp[i]) max = dp[i];
+        }
+
+        System.out.println(max);
+    }
 }
