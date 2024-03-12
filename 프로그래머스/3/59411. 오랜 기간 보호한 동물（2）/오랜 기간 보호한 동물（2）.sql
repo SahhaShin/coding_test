3bac96ad2ebad1ae981dae56c6ea86ba963b4cc2
ShinSanha <33896511+SahhaShin@users.noreply.github.com>
2024-03-13 02:12:20 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -0,0 +1,6 @@
+# 입양을 간 동물 중, 보호 기간이 가장 길었던 동물 두 마리의 아이디와 이름
+# 보호 기간이 긴 순
+SELECT DISTINCT i.ANIMAL_ID, i.NAME
+FROM ANIMAL_INS i JOIN ANIMAL_OUTS o ON i.ANIMAL_ID = o.ANIMAL_ID
+ORDER BY DATEDIFF(o.DATETIME, i.DATETIME) DESC
+LIMIT 2;
