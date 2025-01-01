0d7a6670a13c1daa6d732e55a256fcf44e070b4d
ShinSanha <33896511+SahhaShin@users.noreply.github.com>
2025-01-01 22:06:15 +0900

    [Bronze V] Title: 새싹, Time: 60 ms, Memory: 11356 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("         ").append(",r'\"7").append("\n");
+        sb.append("r`-_").append("   ").append(",'").append("  ").append(",/").append("\n");
+        sb.append(" ").append("\\.").append(" ").append("\".").append(" ").append("L_r'").append("\n");
+        sb.append("   ").append("`~\\/").append("\n");
+        sb.append("      ").append("|").append("\n");
+        sb.append("      ").append("|");
+        System.out.println(sb);
+    }
+}
